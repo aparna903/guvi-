476e09471f5abf63a285bd6c7451e87e2c794bd9
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-09-04 12:18:54 +0530

    Create repeatOfK.java

---
+++ player/repeatOfK.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s1;
+		char a;
+		int m;
+		Scanner sc=new Scanner(System.in);
+		s1=sc.next();
+		a=sc.next().charAt(0);
+		 m=s1.length();
+	for(int i=0;i<m;i++){
+		if(s1.charAt(i)==a){
+			System.out.print(i+1);
+			break;
+		}
+	}
+	
+	           
+	}
+}
