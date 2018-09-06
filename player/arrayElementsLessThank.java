697e30a267253de61a80207268660b06a4148924
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-09-06 15:41:42 +0530

    Create arrayElementsLessThank.java

---
+++ player/arrayElementsLessThank.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			int n;
+		
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+	            int c=0;
+		for(int i=0;i<n;i++){
+			
+		if(a[i]<n){
+			c++;
+			if(c==1)
+			System.out.print(a[i]);
+			else
+			System.out.print(" "+a[i]);
+		}
+		}
+	}
+}
