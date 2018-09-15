98744dfa98a857310e4aea82cc7bd8e1619cb61b
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-09-15 09:35:48 +0530

    Create bitwiseOr.java

---
+++ player/bitwiseOr.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,k=0;
+	
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int i=1;i<n;i++){
+			k=(int)(a[i]|a[i-1]);
+		}
+		System.out.print(k);
+	}
+}
