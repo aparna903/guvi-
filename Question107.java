4a18ba79b04100e06a25356f1958f929933c2b45
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-09-28 12:31:41 +0530

    Create Question107.java

---
+++ Question107.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str1,str2;
+		Scanner sc=new Scanner(System.in);
+		str1=sc.nextLine();
+		String[] s=str1.split(" ");
+		str2=sc.next();
+		for(int i=0;i<s.length;i++){
+		if(s[i].contains(str2)){
+			System.out.print(i+1);
+		}
+		}
+		}
+}
