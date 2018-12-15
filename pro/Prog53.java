d47472f84cb50f8377464bf9bde84be5c0b25c0c
aparna903 <30863910+aparna903@users.noreply.github.com>
2018-12-15 21:08:29 +0530

    Create Prog53.java

---
+++ pro/Prog53.java
@@ -0,0 +1,45 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+s=s.toLowerCase();
+String s1="";
+int count=0;
+for(int i=0;i<s.length();i++)
+{
+String v=String.valueOf(s.charAt(i));
+if(!s1.contains(v))
+{
+s1=s1+v;
+}
+}
+System.out.print(s1);
+for(int i=0;i<s1.length();i++)
+{
+if(((s1.charAt(i)>='a')&&(s1.charAt(i)<='z')))
+{
+count++;
+}
+}
+if(count==26)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+
+
+	}
+}
